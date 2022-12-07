5d77aec0359b5e121761ef554f658e704f92f5df
schong33 <114360892+schong33@users.noreply.github.com>
2022-12-07 15:44:16 -0500

    Revert "Function 2 working. Could improve efficiency if time."

--- function2.R
+++ function2.R
@@ -3,7 +3,6 @@
   # dirPath: string, path to desired directory for function application
   # NAmode: string, accepts "remove" "warn" or "include" commands, otherwise returns an error
   # country: string, country name
-
 compileData=function(dirPath, NAmode="remove", country){
   # set working directory
   setwd(dirPath)
@@ -19,29 +18,24 @@ compileData=function(dirPath, NAmode="remove", country){
   initialData=data.frame()
   
   for(i in 1:length(fileNames)){
-    # import .csv file as new dataframe
     newData=read.csv(fileNames[i],header=TRUE)
-    # initial dataframe contains previously compiled data
     df1=initialData
-    # new dataframe contains new data
     df2=newData
-    # add country column to new data
+    # add country column
     df2$country=(countryVar)
-    # add dayOfYear column to new data
+    # add dayOfYear column
     df2$dayOfYear=(doy[i])
-    # bind new dataframe with previous
+    # bind .csv file with previous data
     allData=rbind(df1, df2)
-    # update initial dataframe to reflect most recent addition
     initialData=allData
   }
   
   # respond to user input
   if (NAmode=="remove"){
-    for (r in 1:nrow(allData)){
-      # search each row for presence of NA values
-      if (anyNA(allData[r,])){
-        # remove rows containing NAs
-        allData=allData[(-r),]
+    # search by row & column for "NA"
+    for (r in allData){
+      if is.na(allData[r]){
+        allData[(-r),]
       }
     }
   }
@@ -52,16 +46,12 @@ compileData=function(dirPath, NAmode="remove", country){
   else if (NAmode=="include"){
     # no change
   }
-  else {
-    # return error
-    print("Invalid input for NAMode")
-  }
   
   return (allData)
 }
 
-directory="/Users/annamccartan/Library/CloudStorage/GoogleDrive-annamac2021@gmail.com/My Drive/School/Fall 2022/Biocomputing/unedited_Rproj/countryX"
-allData=compileData(dirPath=directory, NAmode="remove", country="X")
+directory="/Users/annamccartan/Google Drive/My Drive/School/Fall 2022/Biocomputing/R/RProject/RProject2022/countryX"
+runFunction=compileData(dirPath=directory, NAmode="warn", country="X")
 # make allData dataframe a .csv file
-write.csv(x=allData, file="/Users/annamccartan/Library/CloudStorage/GoogleDrive-annamac2021@gmail.com/My Drive/School/Fall 2022/Biocomputing/Biocomputing_RProject-anna\\allData.csv")
-                
+write.csv(x=runFunction, file="/Users/annamccartan/Google Drive/My Drive/School/Fall 2022/Biocomputing/R/RProject/RProject22/allData.csv")
+
