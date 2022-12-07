c08fcc7a2c9bf72aed558d18cc394d3dba0678e8
Sarah Chong <schong3@nd.edu>
2022-12-07 10:40:21 -0500

    Completed but still editing age plot

--- SupportingFunctions3.R
+++ SupportingFunctions3.R
@@ -1,15 +1,122 @@
 #Biocomputing R Project Notes
 #1. In which country (X or Y) did the disease outbreak likely begin?
 #2. If Country Y develops a vaccine for the disease, is it likely to work for citizens of Country X?
-#presence or absence of ten markers specific to an immunologically active protein
+
 
 #Custom Function 3
-#summarize the compiled data set 
-#number of screens run=count rows
-#percent of patients screened that were infected=patients with at least one marker/total patients
-#male vs. female patients=if male infected male/total male, else female infected female/total female
-#and the age distribution of patients=plot?
 
-DataSet<-read.table("allData.csv",header=TRUE, sep=",", stringsAsFactors=TRUE)
-NumberOfScreens <- length()
+DataSet<-read.table("allData.csv",header=TRUE, sep=",", stringsAsFactors=FALSE)
+
+#Number of screens run can be determined by the number of rows(patients) in the DataSet
+NumberOfScreens <- nrow(DataSet)
+
+
+#Goal: Percent of patients screened that were infected
+#Patients with at least one marker are infected patients
+
+#create variables to hold the number of infected and notinfected patients
+Infected<-0
+NotInfected<-0
+
+#Add up the total number of infected and notinfected patients
+for(i in 1:nrow(DataSet)){
+  SumRow<-sum(DataSet$marker01[i], DataSet$marker02[i],DataSet$marker03[i],DataSet$marker04[i], DataSet$marker05[i],DataSet$marker06[i],DataSet$marker07[i], DataSet$marker08[i],DataSet$marker09[i],DataSet$marker10[i])
+  
+  if(SumRow>0){
+    
+    Infected<-Infected+1
+    
+  }else{
+    
+    if(SumRow==0)
+      NotInfected<-NotInfected+1
+  }
+} 
+
+#Percent of population infected can be determined by Infected Patients/Total number of patients screened
+PercentInfected<-Infected/NumberOfScreens
+
+#Goal: Compарее Мale vs. Female infected patient percentages
+
+#Infected Male Patients
+MaleInfected<-0
+MaleNotInfected<-0
+
+#Add up the total number of Male infected and notinfected patients
+for(i in 1:nrow(DataSet)){
+  if(DataSet$gender[i]=="male"){
+    MSumRow<-sum(DataSet$marker01[i], DataSet$marker02[i],DataSet$marker03[i],DataSet$marker04[i], DataSet$marker05[i],DataSet$marker06[i],DataSet$marker07[i], DataSet$marker08[i],DataSet$marker09[i],DataSet$marker10[i])
+    
+    if(MSumRow>0){
+      
+      MaleInfected<-MaleInfected+1
+      
+    }else{
+      
+      if(MSumRow==0)
+        MaleNotInfected<-MaleNotInfected+1
+    }
+  } 
+  
+}
+MaleTotal=(MaleInfected+MaleNotInfected)
+
+#Percent of Male infected can be determined by Infected Patients/Total number of patients screened
+PercentMaleInfected<-MaleInfected/MaleTotal
+
+#Infected Female Patients
+FemaleInfected<-0
+FemaleNotInfected<-0
+
+#Add up the total number of Male infected and notinfected patients
+for(i in 1:nrow(DataSet)){
+  if(DataSet$gender[i]=="female"){
+    FSumRow<-sum(DataSet$marker01[i], DataSet$marker02[i],DataSet$marker03[i],DataSet$marker04[i], DataSet$marker05[i],DataSet$marker06[i],DataSet$marker07[i], DataSet$marker08[i],DataSet$marker09[i],DataSet$marker10[i])
+    
+    if(FSumRow>0){
+      
+      FemaleInfected<-FemaleInfected+1
+      
+    }else{
+      
+      if(FSumRow==0)
+        FemaleNotInfected<-FemaleNotInfected+1
+    }
+  } 
+  
+}
+FemaleTotal=(FemaleInfected+FemaleNotInfected)
+
+#Percent of Male infected can be determined by Infected Patients/Total number of patients screened
+PercentFemaleInfected<-FemaleInfected/FemaleTotal
+
+
+#Age distribution of all patients plotted
+ggplot(data=DataSet,aes(x=age))+
+  geom_freqpoly()+
+  theme_classic()+
+  xlim(0,100)
+
+#Age distribution of all infected patients plotted
+ggplot(data=DataSet,aes(x=age))+
+  geom_freqpoly()+
+  theme_classic()+
+  xlim(0,100)
+
+
+for(i in 1:nrow(DataSet)){
+  SumRow<-sum(DataSet$marker01[i], DataSet$marker02[i],DataSet$marker03[i],DataSet$marker04[i], DataSet$marker05[i],DataSet$marker06[i],DataSet$marker07[i], DataSet$marker08[i],DataSet$marker09[i],DataSet$marker10[i])
+  
+  if(SumRow>0){
+    
+    InfectedPlotData<-
+      
+    
+  }else{
+    
+    if(SumRow==0)
+      NotInfected<-NotInfected+1
+  }
+} 
+
 
