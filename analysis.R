e9f0f9a179b60146a6121c236082766d2f6d5362
Anna McCartan <amccarta@nd.edu>
2022-12-07 16:38:44 -0500

    analysis needed to create compiled data file

--- analysis.R
+++ analysis.R
@@ -1,22 +1,30 @@
 # read in functions defined in supportingFunctions.R
-functionPath="/Users/annamccartan/Library/CloudStorage/GoogleDrive-annamac2021@gmail.com/My Drive/School/Fall 2022/Biocomputing/R/Biocomputing_RProject/supportingFunctions.R"
+functionPath="/Users/annamccartan/Library/CloudStorage/GoogleDrive-annamac2021@gmail.com/My Drive/School/Fall 2022/Biocomputing/R/Biocomputing_RProject-anna/supportingFunctions.R"
 source(functionPath)
 
-# define directory and storage paths
-Xpath="/Users/annamccartan/Library/CloudStorage/GoogleDrive-annamac2021@gmail.com/My Drive/School/Fall 2022/Biocomputing/R/Biocomputing_RProject/Rproject2022/countryX"
-Ypath="/Users/annamccartan/Library/CloudStorage/GoogleDrive-annamac2021@gmail.com/My Drive/School/Fall 2022/Biocomputing/R/Biocomputing_RProject/Rproject2022/countryY"
-storeX="/Users/annamccartan/Library/CloudStorage/GoogleDrive-annamac2021@gmail.com/My Drive/School/Fall 2022/Biocomputing/R/Biocomputing_RProject/Rproject2022\\XData.csv"
-storeY="/Users/annamccartan/Library/CloudStorage/GoogleDrive-annamac2021@gmail.com/My Drive/School/Fall 2022/Biocomputing/R/Biocomputing_RProject/Rproject2022\\YData.csv"
-
 # use csvConvert to convert country Y files to .csv
-setwd(countryY)
+Ypath="/Users/annamccartan/Library/CloudStorage/GoogleDrive-annamac2021@gmail.com/My Drive/School/Fall 2022/Biocomputing/R/Biocomputing_RProject-anna/Rproject2022/countryY"
+setwd(Ypath)
 csvConvert()
-# use compileData to combine all country X files into one, all country Y files into one
-countryX.csv=compileData(dirPath=Xpath, NAmode="remove", country="X", destPath=storeX)
-countryY.csv=compileData(dirPath=Ypath, NAmode="remove", country="Y", destPath=storeY)
-# combine country X and country Y files
 
-# use function 3 to process the data included in the entire data set
+# use compileData to combine all country X files into one, all country Y files into one
+Xpath="/Users/annamccartan/Library/CloudStorage/GoogleDrive-annamac2021@gmail.com/My Drive/School/Fall 2022/Biocomputing/R/Biocomputing_RProject-anna/Rproject2022/countryX"
+compileData(dirPath=Xpath, NAmode="remove", country="X", dataName="XData.csv")
+Ypath="/Users/annamccartan/Library/CloudStorage/GoogleDrive-annamac2021@gmail.com/My Drive/School/Fall 2022/Biocomputing/R/Biocomputing_RProject-anna/Rproject2022/countryY"
+compileData(dirPath=Ypath, NAmode="remove", country="Y", dataName="YData.csv")
+# combine country X and country Y files
+setwd(Xpath)
+Xdf=read.csv("XData.csv")
+setwd(Ypath)
+Ydf=read.csv("YData.csv")
+compiledData=rbind(Xdf, Ydf)
+dataPath="/Users/annamccartan/Library/CloudStorage/GoogleDrive-annamac2021@gmail.com/My Drive/School/Fall 2022/Biocomputing/R/Biocomputing_RProject-anna/Rproject2022/"
+setwd(dataPath)
+write.csv(compiledData, file="compData.csv")
+
+# use summData to process the data included in the entire data set
+library(ggplot2)
+summData("compData.csv")
 # answer two questions
   # in which country did the disease outbreak likely begin?
   # if country Y develops a vaccine, is it likely to work for country X's citizens?
