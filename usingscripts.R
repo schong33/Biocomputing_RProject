ca63e54e46e259ba1e78acec056fa608e9120192
Sarah Chong <schong3@nd.edu>
2022-12-10 15:05:16 -0500

    updated

--- usingscripts.R
+++ usingscripts.R
@@ -1,11 +1,14 @@
 # read in functions defined in supportingFunctions.R
-source(file)
+source("SupportingFunctions3.R")
+source("function2.R")
+source("RProjectScript.R")
 
 # use function 1 to convert country Y files to .csv
 # use function 2 to combine all country X files into one, all country Y files into one
 # combine country X and country Y files
 
 # use function 3 to process the data included in the entire data set
+
 # answer two questions
   # in which country did the disease outbreak likely begin?
   # if country Y develops a vaccine, is it likely to work for country X's citizens?
